2379bf89a45227cc261a37123f32dd2bbb35697f
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2020-05-25 17:50:16 +0300

    Revert "update connection options help"
    This reverts commit 8c9818f26118f79dacee2f2de14a6ae3a543b3f7.

--- index.js
+++ index.js
@@ -42,7 +42,6 @@ Connection options:
   -p --port=PGPORT                    database server port                          [default: 5432]
   -U --user=PGUSER                    database user name
   -W --password=PGPASSWORD            database user name password
-  -S --ssl=PGSSL                      database connection using ssl                 [default: false]
 `;
 
 const opt = docopt(doc, { version: packagejson.version });
