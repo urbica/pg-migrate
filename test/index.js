2e560f137ee1ac955972ed8539359c96334f6816
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2020-07-02 11:25:56 +0300

    fix: fix cli ssl option parsing (#158)
    re #157

--- test/index.js
+++ test/index.js
@@ -37,7 +37,13 @@ test('migrate', async t => {
   await pgMigrate.migrate();
   await pgMigrate.end();
 
-  const db = pgp({ database, user });
+  const db = pgp({
+    host,
+    port,
+    database,
+    user,
+    password
+  });
 
   await db
     .oneOrNone(tableExistsQuery, { schemaName, tableName })
@@ -57,12 +63,26 @@ test('migrate', async t => {
 test('rollback', async t => {
   t.plan(3);
 
-  const pgMigrate = new PgMigrate({ database, user, migrationsDir });
+  const pgMigrate = new PgMigrate({
+    host,
+    port,
+    database,
+    user,
+    password,
+    migrationsDir
+  });
+
   await pgMigrate.connect();
   await pgMigrate.rollback();
   await pgMigrate.end();
 
-  const db = pgp({ database, user });
+  const db = pgp({
+    host,
+    port,
+    database,
+    user,
+    password
+  });
 
   await db
     .oneOrNone(tableExistsQuery, { schemaName, tableName })
@@ -82,12 +102,26 @@ test('rollback', async t => {
 test('reset', async t => {
   t.plan(3);
 
-  const pgMigrate = new PgMigrate({ database, user, migrationsDir });
+  const pgMigrate = new PgMigrate({
+    host,
+    port,
+    database,
+    user,
+    password,
+    migrationsDir
+  });
+
   await pgMigrate.connect();
   await pgMigrate.reset();
   await pgMigrate.end();
 
-  const db = pgp({ database, user });
+  const db = pgp({
+    host,
+    port,
+    database,
+    user,
+    password
+  });
 
   await db
     .oneOrNone(tableExistsQuery, { schemaName, tableName })
